ae652866467eb7cbaa341b76e6dcdcdafe560057
Jim Lin <jimlintw922@gmail.com>
2015-12-14 22:13:36 +0800

    added create-routers.sh

---
+++ create-routers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <external gateway network id>"
+	exit 1
+fi
+
+source get-api-token.sh
+source get-neutron-network-service-public-url.sh
+
+export RESP_JSON_ROUTERS_CREATE=$(curl -s -X POST $NEUTRON_NETWORK_SERV_PUBLIC_URL/v2.0/routers \
+            -H "Content-Type: application/json" \
+            -H "X-Auth-Token: $API_TOKEN" \
+	    -d "{
+			\"router\": {
+				\"external_gateway_info\": {
+					\"network_id\": \"$1\"
+				},
+				\"name\": \"router-$RANDOM\"
+			}
+		}")
