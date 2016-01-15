977ee3446c45edacb381fa77ff27cc6053229869
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-01-15 12:35:01 +0800

    Update update-init-paas-for-devops.sh

--- init-iaas-for-devops/update-init-paas-for-devops.sh
+++ init-iaas-for-devops/update-init-paas-for-devops.sh
@@ -3,18 +3,34 @@
 set -ex
 
 # to update OpenStackImageID with cloud-image-id
+echo "export UBUNTU_IMAGE_ID=$UBUNTU_IMAGE_ID" >> init.openstack.for-devops.state
 
-# to update OpenStackNetID with network-id
+# to update OpenStackNetID with subnet-id
+echo "export SUBNET_ID=$SUBNET_ID" >> init.openstack.for-devops.state
 
 # to update IaaSVMSSHKeyContent with ssh-private-key-content
+# TODO
 
 # to update OpenStackTenantName with
+echo "export OS_TENANT_NAME=$OS_TENANT_NAME" >> init.openstack.for-devops.state
 # to update OpenStackUserName with
+echo "export OS_USERNAME=$OS_USERNAME" >> init.openstack.for-devops.state
 # to update OpenStackAPIKey with
+echo "export OS_PASSWORD=$OS_PASSWORD" >> init.openstack.for-devops.state
 # to update IaaSVMSSHKeyName with
+echo "export KEYPAIR_NAME=$OS_TENANT_NAME" >> init.openstack.for-devops.state
 # to update OpenStackSecurityGroupID with
+echo "export SECURITY_GROUP_NAME=$OS_TENANT_NAME" >> init.openstack.for-devops.state
 
 # to update API_SERVER
+echo "export INSTALLER_FLOATING_IP=$INSTALLER_FLOATING_IP" >> init.openstack.for-devops.state
+
 # to update OpenStackAuthURL
+echo "export OS_AUTH_URL=$OS_AUTH_URL" >> init.openstack.for-devops.state
+
 # to update OpenStackNetIPPublicMicrobosh
+echo "export MICROBOSH_FLOATING_IP=$MICROBOSH_FLOATING_IP" >> init.openstack.for-devops.state
+
 # to update OpenStackNetIPPublicCF
+echo "export CF_FLOATING_IP=$CF_FLOATING_IP" >> init.openstack.for-devops.state
+
