b913835d69eba8630af864152f8350c58908b94b
Jim Lin <jimlintw922@gmail.com>
2016-01-19 13:09:10 +0000

    updated cleanup-openstack-for-devops.sh

--- init-openstack-for-devops/cleanup-openstack-for-devops.sh
+++ init-openstack-for-devops/cleanup-openstack-for-devops.sh
@@ -11,70 +11,91 @@ STATE_DIR=$2
 
 source $OPENRC_FILE
 
-# to cleanup servers
+function cleanup_microbosh() { 
 	source $STATE_DIR/create-floating-ips.state
 	microbosh=$(nova list | grep $MICROBOSH_FLOATING_IP | awk '{print $2}')
-#		nova delete $microbosh
+		nova delete $microbosh
 		echo "deleted microbosh $microbosh"
 
+	sleep 60
+}
+
+function cleanup_servers() { 
 	for server in $(nova list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		nova delete $server
+		nova delete $server
 		echo "deleted server $server"
 	done
+}
 
-# to cleanup volumes
+function cleanup_volumes() {
 	for volume in $(nova volume-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		nova volume-delete $volume
+		nova volume-delete $volume
 		echo "deleted volume $volume"
 	done
+}
 
-# to cleanup image 
+function cleanup_images () { 
  	for image in $(glance image-list --owner $OS_TENANT_ID | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		glance image-delete $image
+		glance image-delete $image
 		echo "deleted image $image"
 	done
+}
 
-# to cleanup router
-	source $STATE_DIR/create-router-for-devops.state
-#		neutron router-delete $ROUTER_ID
-		echo "deleted router $ROUTER_ID"
-
-# to cleanup network and subnet
-	source $STATE_DIR/create-network-for-devops.state
-#		neutron subnet-delete $SUBNET_ID
-		echo "deleted subnet $SUBNET_ID"
-
-#		neutron net-delete $NETWORK_ID
-		echo "deleted network $NETWORK_ID"
-
-
-# to cleanup ssh key pair
-	for keypair in $(nova keypair-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
-	do
-#		nova keypair-delete $keypair
-		echo "deleted ssh key pair $keypair"
-	done
-
-# to cleanup security group and rules
+function cleanup_security_groups_and_rules() {
 	for secgrouprule in $(neutron security-group-rule-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		neutron security-group-rule-delete $secgrouprule
+		neutron security-group-rule-delete $secgrouprule
 		echo "deleted security group rule $secgrouprule"
 	done
 
 	for secgroup in $(neutron security-group-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		neutron security-group-delete $secgroup
+		neutron security-group-delete $secgroup
 		echo "deleted security group $secgroup"
 	done
+}
 
-# to cleanup floating ips
+function cleanup_ssh_key_pairs() {
+	for keypair in $(nova keypair-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
+	do
+		nova keypair-delete $keypair
+		echo "deleted ssh key pair $keypair"
+	done
+}
+
+function cleanup_floating_ips() {
 	for ip in $(neutron floatingip-list | grep ^\| | sed -n '2,$ p' | awk '{print $2}')
 	do
-#		neutron floatingip-delete $ip
+		neutron floatingip-delete $ip
 		echo "deleted ip $ip"
 	done
+}
 
+function cleanup_network_and_subnet_and_router() {
+	source $STATE_DIR/create-router-for-devops.state
+	source $STATE_DIR/create-network-for-devops.state
+		neutron router-interface-delete $ROUTER_ID $SUBNET_ID
+		echo "deleted interface of router $ROUTER_ID from subnet $SUBNET_ID"
+
+		neutron subnet-delete $SUBNET_ID
+		echo "deleted subnet $SUBNET_ID"
+
+		neutron net-delete $NETWORK_ID
+		echo "deleted network $NETWORK_ID"
+
+		neutron router-delete $ROUTER_ID
+		echo "deleted router $ROUTER_ID"
+}
+
+#main
+cleanup_microbosh
+cleanup_servers 
+cleanup_volumes
+cleanup_images  
+cleanup_security_groups_and_rules
+cleanup_ssh_key_pairs
+cleanup_floating_ips	
+cleanup_network_and_subnet_and_router
