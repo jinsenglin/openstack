f36cedbdf47721c763a611f0ce7a5764c41b7de3
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-01-28 23:43:46 +0800

    Update and rename query-exteral-net-id.sh to query-external-net-id.sh

--- init-openstack-for-devops/query-external-net-id.sh
+++ init-openstack-for-devops/query-external-net-id.sh
@@ -6,7 +6,7 @@ for id in $(neutron net-list | grep "^|" | grep -v "^| id" | awk '{print $2}')
 do
 	neutron net-show $id | tee neutron-net-show.state 
 	if [ "$(grep 'router:external' neutron-net-show.state | awk '{print $4}')" == "True" ]; then
-		echo "export EXTERNAL_NET_ID=$id" > query-exteral-net-id.state 
+		echo "export EXTERNAL_NET_ID=$id" > query-external-net-id.state 
 		exit 0
 	fi
 done
