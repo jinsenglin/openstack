1269d6411ca941fc8346b9fb0266c10eecc5107e
Jim Lin <jimlintw922@gmail.com>
2017-09-13 11:07:58 +0800

    update disk_allocation_ratio.sh

--- sa/disk_allocation_ratio.sh
+++ sa/disk_allocation_ratio.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo community default is 1.0
+
+openstack hypervisor show -c free_disk_gb -c local_gb -c local_gb_used 1
+openstack hypervisor show -c free_disk_gb -c local_gb -c local_gb_used 2
+openstack hypervisor show -c free_disk_gb -c local_gb -c local_gb_used 3
