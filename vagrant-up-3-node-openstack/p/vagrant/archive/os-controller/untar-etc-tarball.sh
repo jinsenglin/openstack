45df02a4d14a8a967b59406c14edb3b43d4deb0a
Jim Lin <jimlintw922@gmail.com>
2017-12-27 23:29:27 +0800

    add untar-etc-tarball.sh

---
+++ vagrant-up-3-node-openstack/p/vagrant/archive/os-controller/untar-etc-tarball.sh
@@ -0,0 +1,13 @@
+set -e
+
+#------------------------------
+
+tar -zxf etc.tgz etc/glance
+tar -zxf etc.tgz etc/keystone
+tar -zxf etc.tgz etc/neutron
+tar -zxf etc.tgz etc/nova
+
+#------------------------------
+
+
+#------------------------------
