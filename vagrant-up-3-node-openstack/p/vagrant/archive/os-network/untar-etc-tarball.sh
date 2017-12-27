d9f7f39487f3078239a6438664eb43ad08e0a589
Jim Lin <jimlintw922@gmail.com>
2017-12-28 00:12:27 +0800

    update archive

--- vagrant-up-3-node-openstack/p/vagrant/archive/os-network/untar-etc-tarball.sh
+++ vagrant-up-3-node-openstack/p/vagrant/archive/os-network/untar-etc-tarball.sh
@@ -9,6 +9,15 @@ tar -zxf etc.tgz etc/neutron
 sed -i '/^#/ d' etc/neutron/neutron.conf
 sed -i '/^$/ d' etc/neutron/neutron.conf
 
+sed -i '/^#/ d' etc/neutron/l3_agent.ini
+sed -i '/^$/ d' etc/neutron/l3_agent.ini
+
+sed -i '/^#/ d' etc/neutron/dhcp_agent.ini
+sed -i '/^$/ d' etc/neutron/dhcp_agent.ini
+
+sed -i '/^#/ d' etc/neutron/metadata_agent.ini
+sed -i '/^$/ d' etc/neutron/metadata_agent.ini
+
 sed -i '/^#/ d' etc/neutron/plugins/ml2/ml2_conf.ini
 sed -i '/^$/ d' etc/neutron/plugins/ml2/ml2_conf.ini
 
