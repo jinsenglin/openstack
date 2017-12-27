d5f5b4c8595b2d6431172f838aa2e7f74fc2a12f
Jim Lin <jimlintw922@gmail.com>
2017-12-28 00:06:16 +0800

    update archive

--- vagrant-up-3-node-openstack/p/vagrant/archive/os-compute/untar-etc-tarball.sh
+++ vagrant-up-3-node-openstack/p/vagrant/archive/os-compute/untar-etc-tarball.sh
@@ -10,7 +10,13 @@ tar -zxf etc.tgz etc/nova
 sed -i '/^#/ d' etc/neutron/neutron.conf
 sed -i '/^$/ d' etc/neutron/neutron.conf
 
+sed -i '/^#/ d' etc/neutron/plugins/ml2/openvswitch_agent.ini
+sed -i '/^$/ d' etc/neutron/plugins/ml2/openvswitch_agent.ini
+
 sed -i '/^#/ d' etc/nova/nova.conf
 sed -i '/^$/ d' etc/nova/nova.conf
+
+sed -i '/^#/ d' etc/nova/nova-compute.conf
+sed -i '/^$/ d' etc/nova/nova-compute.conf
 
 #------------------------------
