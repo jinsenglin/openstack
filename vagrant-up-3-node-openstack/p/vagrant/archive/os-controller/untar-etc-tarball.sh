4a1f1f30d5ac6a8fb7c8d3156aac38171b348a15
Jim Lin <jimlintw922@gmail.com>
2017-12-27 23:33:31 +0800

    update untar-etc-tarball.sh

--- vagrant-up-3-node-openstack/p/vagrant/archive/os-controller/untar-etc-tarball.sh
+++ vagrant-up-3-node-openstack/p/vagrant/archive/os-controller/untar-etc-tarball.sh
@@ -2,6 +2,14 @@ set -e
 
 #------------------------------
 
+tar -zxf etc.tgz etc/apache2
+tar -zxf etc.tgz etc/chrony
+tar -zxf etc.tgz etc/memcached.conf
+tar -zxf etc.tgz etc/mysql
+tar -zxf etc.tgz etc/rabbitmq
+
+#------------------------------
+
 tar -zxf etc.tgz etc/glance
 tar -zxf etc.tgz etc/keystone
 tar -zxf etc.tgz etc/neutron
