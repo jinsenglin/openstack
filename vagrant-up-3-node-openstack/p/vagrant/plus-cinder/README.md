102d1becc59f0e7e8345cfaa3138175ddf3eadc4
Jim Lin <jimlintw922@gmail.com>
2017-12-28 11:02:20 +0800

    update plus-cinder

--- vagrant-up-3-node-openstack/p/vagrant/plus-cinder/README.md
+++ vagrant-up-3-node-openstack/p/vagrant/plus-cinder/README.md
@@ -5,7 +5,7 @@ vagrant ssh os-controller -c "sudo /vagrant/update-os-controller.sh download-cin
 vagrant up --provision-with download os-block
 
 vagrant ssh os-controller -c "sudo /vagrant/update-os-controller.sh configure-cinder"
-vagrant ssh os-block -c "sudo /vagrant/update-os-controller.sh configure-cinder"
+vagrant ssh os-block -c "sudo /vagrant/bootstrap-os-block.sh configure"
 ```
 
 
--- vagrant-up-3-node-openstack/p/vagrant/plus-cinder/bootstrap-os-block.sh
+++ vagrant-up-3-node-openstack/p/vagrant/plus-cinder/bootstrap-os-block.sh
@@ -12,8 +12,6 @@ ENV_TUNNEL_OS_CONTROLLER_IP="10.0.1.11"
 ENV_TUNNEL_OS_NETWORK_IP="10.0.1.21"
 ENV_TUNNEL_OS_COMPUTE_IP="10.0.1.31"
 
-ENV_PUBLIC_INTERFACE="enp0s10"
-
 LOG=/tmp/provision.log
 date | tee $LOG            # when:  Thu Aug 10 07:48:13 UTC 2017
 whoami | tee -a $LOG       # who:   root
@@ -62,6 +60,47 @@ DATA
     # Reference https://docs.openstack.org/newton/install-guide-ubuntu/environment-networking.html
 }
 
+function install_utilities() {
+    [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
+    apt-get install -y crudini
+}
+
+function install_python() {
+    PYTHON_VERSION=2.7.11-1
+    PYTHON_PIP_VERSION=8.1.1-2ubuntu0.4
+    [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
+    #apt-get install -y python=$PYTHON_VERSION python-pip=$PYTHON_PIP_VERSION
+    apt-get install -y python python-pip
+}
+
+function install_ntp() {
+    CHRONY_VERSION=2.1.1-1
+    [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
+    #apt-get install -y chrony=$CHRONY_VERSION
+    apt-get install -y chrony
+
+    # # # # # # # # # # # # # # # # ## # # # # # # # # # # # # # # # # # # # # # # # # ## # # # # # # # #
+
+    # To connect to the os-controller node
+    sed -i "s/^pool /#pool /g" /etc/chrony/chrony.conf
+    sed -i "s/^server /#server /g" /etc/chrony/chrony.conf
+    echo "server os-controller iburst" >> /etc/chrony/chrony.conf
+
+    # Restart the NTP service
+    service chrony restart
+
+    # Verify operation
+    chronyc sources
+
+    # Log files
+    # /var/log/chrony/measurements.log
+    # /var/log/chrony/statistics.log
+    # /var/log/chrony/tracking.log
+
+    # Reference https://docs.openstack.org/newton/install-guide-ubuntu/environment-ntp-other.html
+}
+
+
 function download_cinder() {
     [ "$APT_UPDATED" == "true" ] || apt-get update && APT_UPDATED=true
     apt-get install -y cinder-volume
@@ -79,6 +118,9 @@ function main() {
                 #use_local_apt_server
                 use_public_apt_server
                 each_node_must_resolve_the_other_nodes_by_name_in_addition_to_IP_address
+                install_utilities
+                install_python
+                install_ntp
                 download_cinder
                 ;;
             configure)
